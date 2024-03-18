7b79ab4bcf14f46db21ed424743f94ee2056647f
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2024-03-18 10:54:44 +0000

    Update

--- C++/C++ Project/MyC.cc
+++ C++/C++ Project/MyC.cc
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <cctype>
+
 using namespace std;
+
+int Login()
+{
+    
+}
+
+int SignUp()
+{
+}
 
 int main()
 {
+    cout << "Do You Have An Account? ";
+    string Data << cin << endl;
+    std::transform(Data.begin(), Data.end(), Data.begin(),
+    [](unsigned char c){ return std::tolower(c); });
+    if (Data == "Y")
+    {
+        SignUp();
+    }else
+    {
+        Login();
+    }
     return 0;
 }
