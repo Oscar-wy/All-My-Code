ddaff9bdf7b6e790e4d4f839ae8cec997c784f95
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2024-03-22 11:31:01 +0000

    Update C

--- C++/C++ Project/MyC.cc
+++ C++/C++ Project/MyC.cc
@@ -7,25 +7,27 @@ using namespace std;
 
 int Login()
 {
-    
+    cout << "Login" << endl;
 }
 
 int SignUp()
 {
+    cout << "SignUp" << endl;
 }
 
 int main()
 {
     cout << "Do You Have An Account? ";
-    string Data << cin << endl;
+    string Data;
+    cin >> Data;
     std::transform(Data.begin(), Data.end(), Data.begin(),
     [](unsigned char c){ return std::tolower(c); });
-    if (Data == "Y")
-    {
-        SignUp();
-    }else
+    if (Data == "y")
     {
         Login();
+    }else
+    {
+        SignUp();
     }
     return 0;
 }
