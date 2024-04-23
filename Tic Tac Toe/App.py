ef27d8372334fc9314c675cab2ed9df609dca6df
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2024-04-23 13:57:42 +0000

    d

---
+++ Tic Tac Toe/App.py
@@ -0,0 +1,16 @@
+class Player():
+    def __init__(self, char):
+        self.Character = char
+        self.Wins = 0
+
+P1 = Player("X")
+P2 = Player("O")
+Stop = False
+Current = P1.Character
+
+def Tictactoe():
+    while Stop == False:
+        pass
+
+if __name__ == "__main__":
+    Tictactoe()
