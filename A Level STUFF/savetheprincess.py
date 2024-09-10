f539b2b53cc6e6e19380d85b37bf12dc994d09a8
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2024-09-10 10:24:56 +0000

    Finished The Save The Princess Code

--- A Level STUFF/savetheprincess.py
+++ A Level STUFF/savetheprincess.py
@@ -7,7 +7,7 @@ while size%2==0:
 grid=[["-" for x in range(size)] for y in range(size)]
 mid=(size//2)
 grid[mid][mid]="m"
-grid[randint(0,size)][randint(0,size)]="p"
+grid[randint(0,size-1)][randint(0,size-1)]="p"
 
 for row in grid:
     for e in row:
@@ -16,8 +16,34 @@ for row in grid:
 
 print("Maurizio needs to rescue the Princess")
 
+maurCoords = []
+prinCoords = []
+
 for i in range(len(grid)):
     print(grid[i])
+    for j in range(len(grid[i])):
+        if grid[i][j] == "p":
+            prinCoords = [i, j]
+        if grid[i][j] == "m":
+            maurCoords = [i, j]
+
+if maurCoords[0] > prinCoords[0]:
+    coordValue = maurCoords[0] - prinCoords[0]
+    for i in range(coordValue):
+        print("UP")
+else:
+    coordValue = prinCoords[0] - maurCoords[0]
+    for i in range(coordValue):
+        print("DOWN")
+
+if maurCoords[1] > prinCoords[1]:
+    coordValue = maurCoords[1] - prinCoords[1]
+    for i in range(coordValue):
+        print("LEFT")
+else:
+    coordValue = prinCoords[1] - maurCoords[1]
+    for i in range(coordValue):
+        print("RIGHT")
 
 
 #you need to write the code to solve the puzzle
