1f5b83e1e0ff4def39ee7ecf8c92dbf52b130a39
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2024-10-07 11:51:18 +0000

    Updates

--- Fun/BralfieBrant/Bralfie.py
+++ Fun/BralfieBrant/Bralfie.py
@@ -12,8 +12,10 @@ if __name__ == "__main__":
         currentDay = Player.Day
         if currentDay == Player.Day:
             Player.printMessage()
-        Choice = input("What Do You Want To Do Today? ")
-        if Choice.lower() == "invest":
+        Choice = input("What Do You Want To Do Today? ").lower()
+        if Choice == "invest":
             Invest()
+        elif Choice == "repair" or Choice == "fix":
+            Player.repairLaptop()
         else:
             print("Not A Part Of The Game?")
--- Fun/BralfieBrant/GameFunctions.py
+++ Fun/BralfieBrant/GameFunctions.py
@@ -7,6 +7,10 @@ class Player():
         self.Day = 0
         self.LocalSave = 0
         self.amountInvestedToday = 0
+        self.Repaired = False
+        self.repairCost = 50000
+        self.Username = ""
+        self.localSave = ""
         self.Messages = [f'It\'s day {self.Day} lets get started.', f'Rise and shine it\'s day {self.Day} time to start investing.', f'GooPlayer.Day it\'s day {self.Day}. Get the flippity dippity flop up and start investing YOU LAZY BASTARD!', 'Good morning sunshine, invest don\'t rest and get to it', f'Day {self.Day} wow look how far we\'ve come, tired of investing yet? I DONT CARE DO YOU WANT YOUR LAPTOP FIXED OR NOT!','Get up! Your snooze button isn\'t going to press itself.',f'WAKE UP! Its day {self.Day}, your pillow is begging for a break seriously.', f'Morning, it\'s day {self.Day}! Your bed might miss you but the world won\'t. I promise.']
     def Invest(self, selection, amount):
         currentStockPrice = selection.stockValue
@@ -20,9 +24,32 @@ class Player():
             print("You Now Own", amount, "Stocks Of", selection.Name)
         else:
             print("Not Enough Money!")
+    def repairLaptop(self):
+        choice = input(f"The Repair Costs {self.repairCost} Are You Sure (y/n)? ").lower()
+        if choice == "y":
+            if self.Money >= self.repairCost and self.repairCost != 0:
+                self.Money - self.repairCost
+                self.repairCost = 0
+                print("Congrats You Finally Fixed Your Shitty Laptop")
+            elif self.repairCost == 0:
+                print("You Have Already Repaired The Laptop")
+            else:
+                print("You Do Not Have Enough Money To Repair The Laptop.")
+        else:
+            print("\nShouldnt Have Bothered Me")
     def printMessage(self):
         message = random.randint(0, len(self.Messages)-1)
         print(self.Messages[message])
+    def createLocal(self):
+        text = ""
+        try:
+            with open(f".UserSaves/UserSave{Player.Username}", "r+") as file:
+                text = file.readline()
+                file.close()
+        except:
+            open(f".UserSaves/UserSave{Player.Username}", "x")
+        if text == "":
+            self.localSave()
     
 class Investment:
     def __init__(self, name):
