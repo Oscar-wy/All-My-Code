5122f046f4079cdf7417f182d3edb82bcdbb3938
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2024-10-21 13:26:58 +0100

    Delete GCSE STUFF directory

--- GCSE STUFF/Python/Classes.py
+++
@@ -1,6 +0,0 @@
-class MyClass():
-    def __init__(self, *List):
-        for i in range(len(List)):
-            self.name = List[i]
-
-p1 = MyClass("Data1", "Data2")
--- GCSE STUFF/Python/Functions.py
+++
@@ -1,5 +0,0 @@
-def Function(*List):
-    for i in range(len(List)):
-        print(List[i])
-
-Function("Hello", 2, "Test", 284.24)
