7bc6d91ac942e188f89da45b6e761d6c6ab7ae99
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2024-11-18 21:53:55 +0000

    Updates

--- PyWeb.v.2/App.py
+++ PyWeb.v.2/App.py
@@ -3,10 +3,11 @@ import Server
 
 app = Flask(__name__)
 user = Server.User()
+Server.InitialiseTables()
 
 def CheckUser():
     SessionID = request.cookies.get("SessionID")
-    if user.Got:
+    if user.AssignUser(SessionID):
         return True
     return False
 
@@ -23,17 +24,37 @@ def Auth():
         return redirect("/")
     return render_template("Auth.html", ShowMain="Show", ShowSignup="Hidden", ShowLogin="Hidden")
 
-@app.route("/auth/signup")
+@app.route("/auth/signup", methods=["GET", "POST"])
 def Signup():
     if CheckUser():
         return redirect("/")
+    if request.method == "POST":
+        if user.CreateUser(Username=request.form["Username"], FName=request.form["FName"], LName=request.form["LName"], Email=request.form["Email"], Password=request.form["SignPassword"]):
+            resp = make_response(redirect("/"))
+            resp.set_cookie("SessionID", user.SessionID)
+            return resp
     return render_template("Auth.html", ShowMain="Hidden", ShowSignup="Show", ShowLogin="Hidden")
 
-@app.route("/auth/login")
+@app.route("/auth/login", methods=["GET", "POST"])
 def Login():
     if CheckUser():
         return redirect("/")
+    if request.method == "POST":
+        if user.Login(UserEmail=request.form["UserEmail"], Password=request.form["Password"]):
+            resp = make_response(redirect("/"))
+            resp.set_cookie("SessionID", user.SessionID)
+            return resp
     return render_template("Auth.html", ShowMain="LogHidden", ShowSignup="Hidden", ShowLogin="Show")
+
+@app.route("/logout/<UUID>")
+def Logout(UUID):
+    CheckUser()
+    print(UUID, "UserId: ", user.UUID)
+    if UUID == user.UUID:
+        resp = make_response(redirect("/"))
+        resp.delete_cookie('SessionID')
+        return resp
+    return redirect("/")
 
 @app.errorhandler(404)
 def pageNotFound(error):
--- PyWeb.v.2/Server.py
+++ PyWeb.v.2/Server.py
@@ -6,13 +6,14 @@ def InitialiseTables():
         with sqlite3.connect("Data.db") as db:
             cursor = db.cursor()
             sql = """CREATE TABLE IF NOT EXISTS User(
-                     UUID TEXT UNIQUE
-                     NID TEXT UNIQUE
-                     FName varchar(20)
-                     LName varchar(20)
-                     Email TEXT UNIQUE
-                     Password TEXT UNIQUE
-                     PRIMARY KEY(UUID))
+                     UUID TEXT UNIQUE,
+                     NID TEXT UNIQUE,
+                     FName varchar(20),
+                     LName varchar(20),
+                     Email TEXT UNIQUE,
+                     Password TEXT,
+                     SessionID TEXT UNIQUE,
+                     PRIMARY KEY(UUID));
             """
             cursor.execute(sql)
             db.commit()
@@ -28,17 +29,17 @@ class User():
         self.Email = ""
         self.Password = ""
         self.SessionID = ""
-        self.Got = False
     def CheckUsernameExists(self, Username):
         Values = (Username,)
         try:
             with sqlite3.connect("Data.db") as db:
                 cursor = db.cursor()
-                sql = """SELECT Password FROM User
+                sql = """SELECT NID FROM User
                          WHERE NID = ?
                 """
                 cursor.execute(sql, Values)
                 result = cursor.fetchone()
+                print(result)
                 if Username == result:
                     return False
                 return True
@@ -46,19 +47,72 @@ class User():
             print(err)
             return True
     def SetUser(self):
+        try:
+            Values = (self.UUID, self.NID, self.FName, self.LName, self.Email, self.Password, self.SessionID)
+            with sqlite3.connect("Data.db") as db:
+                cursor = db.cursor()
+                sql = """INSERT INTO User(UUID, NID, FName, LName, Email, Password, SessionID)
+                         Values (?, ?, ?, ?, ?, ?, ?)
+                """
+                cursor.execute(sql, Values)
+                return True
+        except sqlite3.Error as err:
+            print(err)
+            return False
     def CreateUser(self, Username, FName, LName, Email, Password):
         try:
             if self.CheckUsernameExists(Username):
-                self.UUID = str(uuid.uuid4).replace("-","")
+                self.UUID = str(uuid.uuid4()).replace("-","")
                 self.NID = Username
                 self.FName = FName
                 self.LName = LName
                 self.Email = Email
                 self.Password = Password
-                self.SessionID = str(uuid.uuid4).replace("-","")
+                self.SessionID = str(uuid.uuid4()).replace("-","")
                 if self.SetUser():
                     return True
                 return False
         except:
             return False
+    def CheckPassword(self, Username, Password):
+        try:
+            Values = (Username, Username)
+            with sqlite3.connect("Data.db") as db:
+                cursor = db.cursor()
+                sql = """SELECT Password FROM User
+                         WHERE NID = ? OR Email = ?
+                """
+                cursor.execute(sql, Values)
+                result, = cursor.fetchone()
+                if result == Password:
+                    return True
+                return False
+        except sqlite3.Error as err:
+            print(err)
+            return False
+    def AssignUser(self, Input):
+        try:
+            Values = (Input, Input, Input)
+            with sqlite3.connect("Data.db") as db:
+                cursor = db.cursor()
+                sql = """SELECT * FROM User
+                         WHERE NID = ? OR Email = ? OR SessionID = ?
+                """
+                cursor.execute(sql, Values)
+                result = cursor.fetchone()
+                self.UUID = result[0]
+                self.NID = result[1]
+                self.FName = result[2]
+                self.LName = result[3]
+                self.Email = result[4]
+                self.Password = result[5]
+                self.SessionID = result[6]
+                return True
+        except:
+            return False
+    def Login(self, UserEmail, Password):
+        if self.CheckPassword(UserEmail, Password):
+            if self.AssignUser(UserEmail):
+                return True
+        return False
     
