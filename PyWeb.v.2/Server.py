b2d2fa902405a2771a038190ddd5f74b72295bd6
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2024-11-18 15:03:30 +0000

    Update

--- PyWeb.v.2/Server.py
+++ PyWeb.v.2/Server.py
@@ -1,5 +1,27 @@
+import sqlite3
+import uuid
+
+def InitialiseTables():
+    try:
+        with sqlite3.connect("Data.db") as db:
+            cursor = db.cursor()
+            sql = """CREATE TABLE IF NOT EXISTS User(
+                     UUID TEXT UNIQUE
+                     NID TEXT UNIQUE
+                     FName varchar(20)
+                     LName varchar(20)
+                     Email TEXT UNIQUE
+                     Password TEXT UNIQUE
+                     PRIMARY KEY(UUID))
+            """
+            cursor.execute(sql)
+            db.commit()
+    except sqlite3.Error as err:
+        print(err)
+
 class User():
     def __init__(self):
+        self.UUID = ""
         self.NID = ""
         self.FName = ""
         self.LName = ""
@@ -7,3 +29,36 @@ class User():
         self.Password = ""
         self.SessionID = ""
         self.Got = False
+    def CheckUsernameExists(self, Username):
+        Values = (Username,)
+        try:
+            with sqlite3.connect("Data.db") as db:
+                cursor = db.cursor()
+                sql = """SELECT Password FROM User
+                         WHERE NID = ?
+                """
+                cursor.execute(sql, Values)
+                result = cursor.fetchone()
+                if Username == result:
+                    return False
+                return True
+        except sqlite3.Error as err:
+            print(err)
+            return True
+    def SetUser(self):
+    def CreateUser(self, Username, FName, LName, Email, Password):
+        try:
+            if self.CheckUsernameExists(Username):
+                self.UUID = str(uuid.uuid4).replace("-","")
+                self.NID = Username
+                self.FName = FName
+                self.LName = LName
+                self.Email = Email
+                self.Password = Password
+                self.SessionID = str(uuid.uuid4).replace("-","")
+                if self.SetUser():
+                    return True
+                return False
+        except:
+            return False
+    
