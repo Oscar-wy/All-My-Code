a913ceab6006c8a537ce616219c312e6b37c93d6
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2024-12-09 10:43:46 +0000

    Updates

--- Olympiad/Practice/Q1.py
+++ Olympiad/Practice/Q1.py
@@ -1,17 +1,25 @@
 def CreatePattern(start, end):
-    stri = []
+    index = 0
+    stri = ""
     for i in range(start, end+1):
-        stri.append(str(i))
+        index += 1
+        stri += str(start+index)
     print(stri)
-    return stri
+    reqd = int(input("Enter position: "))
+    print(stri[reqd+1])
+    FindPattern(stri)
 
-def FindPattern(pattern, rs):
+def FindPattern(pattern):
+    seen = 0
     patternLen = len(pattern)
     for i in range(patternLen):
-        if pattern[i] == rs:
-            print(i, pattern[i])
+        if i >= 101:
+            break
+        print(pattern[i])
+        if pattern[i] == "5":
+            seen += 1
+    print(seen)
 
 start = int(input(">"))
 end = int(input(">"))
 stri = CreatePattern(start, end)
-FindPattern(stri, 999)
