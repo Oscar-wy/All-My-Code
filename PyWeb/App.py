41c7f4cb26a8847fdf6ecf3f7ea044a0992e475b
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2025-05-01 12:22:07 +0000

    Upd

--- PyWeb/App.py
+++ PyWeb/App.py
@@ -6,4 +6,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def Index():
+    SessionID = request.cookies.get("SessionID")
+    if SessionID:
+        UserID = None
+        return redirect("/App")
     return render_template("Landing.html")
+
+@app.route("/App", methods=["GET", "POST"])
+def App():
+    pass
