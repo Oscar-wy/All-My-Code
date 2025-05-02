6ede1720a7d6b1a4f38a5b82aab0ca010f76fcc0
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2025-05-02 09:05:08 +0000

    Upd

--- PyWeb/App.py
+++ PyWeb/App.py
@@ -1,16 +1,25 @@
 from flask import *
 import random
-import Backend.Server
+import Backend.Server as Server
 
 app = Flask(__name__)
 
+# user = Server.User()
+
+# def CheckUser():
+#     SessionID = request.cookies.get("SessionID")
+#     if user.AssignUser(SessionID):
+#         return True
+#     return False
+
+account = False
+
 @app.route("/", methods=["GET", "POST"])
 def Index():
-    SessionID = request.cookies.get("SessionID")
-    if SessionID:
-        UserID = None
-        return redirect("/App")
-    return render_template("Landing.html")
+    if account:
+        #return redirect("/App")
+        return render_template("Landing.html", HasAccount="", NoAccount="Hidden")
+    return render_template("Landing.html", HasAccount="Hidden", NoAccount="")
 
 @app.route("/App", methods=["GET", "POST"])
 def App():
