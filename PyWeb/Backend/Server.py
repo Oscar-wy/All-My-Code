b3c2c77a30f1afe8d8324e71b207b3c5f74fdd9c
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2025-05-02 09:40:29 +0000

    Upd

---
+++ PyWeb/Backend/Server.py
@@ -0,0 +1,13 @@
+import sqlite3
+import random
+
+def InitialiseTables():
+    pass
+
+class User():
+    def __init__(self):
+        self.NID = ""
+        self.Username = ""
+        self.Name = ""
+        self.Email = ""
+        self.Password =  ""
