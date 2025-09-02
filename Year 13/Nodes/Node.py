b12c8a9aa244a43866fcdf248e92a860d0ea24cb
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2025-09-02 14:06:16 +0000

    Update

---
+++ Year 13/Nodes/Node.py
@@ -0,0 +1,38 @@
+class Node:
+    """A node of a linked list"""
+
+    def __init__(self, node_data):
+        self._data = node_data
+        self._next = None
+
+    def get_data(self):
+        """Get node data"""
+        return self._data
+
+    def set_data(self, node_data):
+        """Set node data"""
+        self._data = node_data
+
+    data = property(get_data, set_data)
+
+    def get_next(self):
+        """Get next node"""
+        return self._next
+
+    def set_next(self, node_next):
+        """Set next node"""
+        self._next = node_next
+
+    next = property(get_next, set_next)
+
+    def __str__(self):
+        """String"""
+        return str(self._data)
+    
+newNode = Node("James is a stinky guy")
+newNode2 = Node("James still stinks")
+print(newNode.get_data())
+newNode.set_next(newNode2)
+print(newNode.get_next())
+print(newNode)
+print(newNode2)
