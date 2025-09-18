1851d47faeea5230f76f389f6dcc7392299635e5
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2025-09-18 11:19:13 +0000

    Upd

---
+++ Year 13/Searching Algorithms/Searching Algorithms Task.py
@@ -0,0 +1,26 @@
+x = 10
+
+def linearSearch(searchList, searchVal):
+	for i in searchList:
+		if i == searchVal:
+			return i
+	return ("Value not found")
+
+def binarySearch(searchList, searchVal):
+	start = 0
+	end = len(searchList) - 1
+	while start <= end:
+		mid = (start + end) // 2
+		print(mid)
+		if searchList[mid] == searchVal:
+			return mid
+		elif searchList[mid] < searchVal:
+			start = mid
+		elif searchList[mid] > searchVal:
+			end = mid
+	return ("Value not found")
+
+searchList = [1,2,3,4,5,6,7,8,9,10]
+print(linearSearch(searchList, x))
+print(binarySearch(searchList, x))
+input()
